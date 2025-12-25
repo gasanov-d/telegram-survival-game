d62b6e98adc4c8e20835dc7e8fb6efd9fbbe209c
gasanov-d <gasanovali1999@gmail.com>
2025-12-25 05:46:50 +0300

    Update GameScene.js

--- scenes/GameScene.js
+++ scenes/GameScene.js
@@ -9,29 +9,11 @@ export default class GameScene extends Phaser.Scene {
     const w = this.scale.width;
     const h = this.scale.height;
 
-    // фон
-    this.cameras.main.setBackgroundColor('#6fa8dc');
-
-    // физика мира
-    this.physics.world.setBounds(0, 0, 100000, h);
-
     // платформы
     this.platforms = this.physics.add.staticGroup();
-
-    this.platforms
-      .create(400, h - 20)
-      .setDisplaySize(800, 40)
-      .refreshBody();
-
-    this.platforms
-      .create(900, h - 150)
-      .setDisplaySize(300, 30)
-      .refreshBody();
-
-    this.platforms
-      .create(1400, h - 250)
-      .setDisplaySize(300, 30)
-      .refreshBody();
+    this.platforms.create(w / 2, h - 20).setDisplaySize(w, 40).refreshBody();
+    this.platforms.create(400, h - 150).setDisplaySize(300, 30).refreshBody();
+    this.platforms.create(800, h - 300).setDisplaySize(300, 30).refreshBody();
 
     // игрок
     this.player = this.add.rectangle(100, h - 200, 40, 40, 0xff0000);
@@ -41,7 +23,6 @@ export default class GameScene extends Phaser.Scene {
     this.player.body.setBounce(0);
     this.player.body.setGravityY(900);
 
-    // столкновения
     this.physics.add.collider(this.player, this.platforms);
 
     // камера
@@ -51,22 +32,19 @@ export default class GameScene extends Phaser.Scene {
     // управление
     this.controls = new MobileControls(this);
 
-    // постоянное движение вперёд (survival)
+    // автодвижение (survival)
     this.player.body.setVelocityX(180);
   }
 
   update() {
     const body = this.player.body;
 
-    // движение вперёд всегда
     body.setVelocityX(180);
 
-    // прыжок
     if (this.controls.jump && body.blocked.down) {
       body.setVelocityY(-520);
     }
 
-    // смерть при падении
     if (this.player.y > this.scale.height + 300) {
       this.scene.restart();
     }
