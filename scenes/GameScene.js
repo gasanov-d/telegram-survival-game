4f32fd187044a57c439173052d31846f12e4df32
gasanov-d <gasanovali1999@gmail.com>
2025-12-25 04:49:48 +0300

    Update GameScene.js

--- scenes/GameScene.js
+++ scenes/GameScene.js
@@ -9,26 +9,12 @@ export default class GameScene extends Phaser.Scene {
     const w = this.scale.width;
     const h = this.scale.height;
 
-    // фон
     this.cameras.main.setBackgroundColor('#6fa8dc');
 
-    // платформы
     this.platforms = this.physics.add.staticGroup();
+    this.platforms.create(w / 2, h - 20).setDisplaySize(w, 40).refreshBody();
 
-    this.platforms.create(w / 2, h - 20)
-      .setDisplaySize(w, 40)
-      .refreshBody();
-
-    this.platforms.create(w / 2 - 150, h - 150)
-      .setDisplaySize(250, 30)
-      .refreshBody();
-
-    this.platforms.create(w / 2 + 200, h - 300)
-      .setDisplaySize(250, 30)
-      .refreshBody();
-
-    // игрок
-    this.player = this.add.rectangle(100, h - 200, 40, 40, 0xffcc00);
+    this.player = this.add.rectangle(100, h - 200, 40, 40, 0xff0000);
     this.physics.add.existing(this.player);
 
     this.player.body.setCollideWorldBounds(true);
@@ -36,14 +22,12 @@ export default class GameScene extends Phaser.Scene {
 
     this.physics.add.collider(this.player, this.platforms);
 
-    // управление
     this.controls = new MobileControls(this);
   }
 
   update() {
     const body = this.player.body;
 
-    // горизонтальное движение
     if (this.controls.left) {
       body.setVelocityX(-250);
     } else if (this.controls.right) {
@@ -52,7 +36,6 @@ export default class GameScene extends Phaser.Scene {
       body.setVelocityX(0);
     }
 
-    // прыжок
     if (this.controls.jump && body.blocked.down) {
       body.setVelocityY(-500);
     }
