0027a6ebcde0c7ed355fd1d57758be5cc648f92f
gasanov-d <gasanovali1999@gmail.com>
2025-12-25 05:33:43 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
+import { GAME_CONFIG } from './config/gameConfig.js';
 import BootScene from './scenes/BootScene.js';
 import GameScene from './scenes/GameScene.js';
 
 const config = {
   type: Phaser.AUTO,
-  parent: 'game',
-  width: window.innerWidth,
-  height: window.innerHeight,
+  width: GAME_CONFIG.size.width,
+  height: GAME_CONFIG.size.height,
+  backgroundColor: '#6fa8dc',
   physics: {
     default: 'arcade',
     arcade: {
-      gravity: { y: 900 },
-      debug: false
-    }
+      gravity: { y: GAME_CONFIG.physics.gravity },
+      debug: false,
+    },
   },
-  scene: [BootScene, GameScene]
+  scene: [BootScene, GameScene],
 };
 
 new Phaser.Game(config);
