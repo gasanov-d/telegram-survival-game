b4060d1dbb9316a119cace730d02ed09b96622be
gasanov-d <gasanovali1999@gmail.com>
2025-12-25 03:52:50 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,7 @@
+import { gameConfig } from './config/gameConfig.js';
+
 const tg = window.Telegram.WebApp;
 tg.ready();
 tg.expand();
 
-const config = {
-  type: Phaser.AUTO,
-  parent: 'game',
-  width: window.innerWidth,
-  height: window.innerHeight,
-  backgroundColor: '#6fa8dc',
-  physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: { y: 1200 },
-      debug: false
-    }
-  },
-  scene: [BootScene, MenuScene, GameScene]
-};
-
-new Phaser.Game(config);
+new Phaser.Game(gameConfig);
