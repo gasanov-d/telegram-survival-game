c3f1ffcdf7a330d373240651bb5e6a7f2c77c22c
gasanov-d <gasanovali1999@gmail.com>
2025-12-25 05:57:19 +0300

    Update GameScene.js

--- scenes/GameScene.js
+++ scenes/GameScene.js
@@ -1,4 +1,5 @@
 import MobileControls from './MobileControls.js';
+import PlatformManager from '../world/PlatformManager.js';
 
 export default class GameScene extends Phaser.Scene {
   constructor() {
@@ -9,42 +10,52 @@ export default class GameScene extends Phaser.Scene {
     const w = this.scale.width;
     const h = this.scale.height;
 
-    // платформы
-    this.platforms = this.physics.add.staticGroup();
-    this.platforms.create(w / 2, h - 20).setDisplaySize(w, 40).refreshBody();
-    this.platforms.create(400, h - 150).setDisplaySize(300, 30).refreshBody();
-    this.platforms.create(800, h - 300).setDisplaySize(300, 30).refreshBody();
+    // фон
+    this.cameras.main.setBackgroundColor('#6fa8dc');
+
+    // границы мира (очень широкие)
+    this.physics.world.setBounds(0, 0, 100000, h);
 
     // игрок
     this.player = this.add.rectangle(100, h - 200, 40, 40, 0xff0000);
     this.physics.add.existing(this.player);
 
-    this.player.body.setCollideWorldBounds(false);
-    this.player.body.setBounce(0);
     this.player.body.setGravityY(900);
-
-    this.physics.add.collider(this.player, this.platforms);
-
-    // камера
-    this.cameras.main.startFollow(this.player);
-    this.cameras.main.setDeadzone(100, 150);
+    this.player.body.setCollideWorldBounds(false);
 
     // управление
     this.controls = new MobileControls(this);
 
-    // автодвижение (survival)
+    // платформы (БЕСКОНЕЧНЫЕ)
+    this.platformManager = new PlatformManager(this);
+    this.physics.add.collider(
+      this.player,
+      this.platformManager.platforms
+    );
+
+    // камера
+    this.cameras.main.startFollow(this.player);
+    this.cameras.main.setDeadzone(120, 150);
+
+    // автодвижение вперёд
     this.player.body.setVelocityX(180);
   }
 
   update() {
     const body = this.player.body;
 
+    // постоянное движение
     body.setVelocityX(180);
 
+    // прыжок
     if (this.controls.jump && body.blocked.down) {
       body.setVelocityY(-520);
     }
 
+    // обновление платформ
+    this.platformManager.update();
+
+    // смерть при падении
     if (this.player.y > this.scale.height + 300) {
       this.scene.restart();
     }
