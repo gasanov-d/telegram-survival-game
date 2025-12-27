378fbf8236ad3a59aa8e442ae3a89fac13fa8d6d
gasanov-d <gasanovali1999@gmail.com>
2025-12-27 20:15:56 +0300

    Update GameScene.js

--- scenes/GameScene.js
+++ scenes/GameScene.js
@@ -1,68 +1,26 @@
-import MobileControls from '../controls/MobileControls.js';
+import Player from "../entities/Player.js";
+import InputSystem from "../systems/InputSystem.js";
+import HUD from "../ui/HUD.js";
 
 export default class GameScene extends Phaser.Scene {
   constructor() {
-    super('GameScene');
+    super("GameScene");
   }
 
   create() {
-    const w = this.scale.width;
-    const h = this.scale.height;
+    this.inputSystem = new InputSystem(this);
 
-    // фон
-    this.cameras.main.setBackgroundColor('#6fa8dc');
+    this.player = new Player(this, 400, 300);
 
-    // границы мира
-    this.physics.world.setBounds(0, 0, 10000, h);
-
-    // платформа (земля)
-    this.platforms = this.physics.add.staticGroup();
-    this.platforms
-      .create(5000, h - 20)
-      .setDisplaySize(10000, 40)
-      .refreshBody();
-
-    // игрок
-    this.player = this.add.rectangle(100, h - 100, 40, 40, 0xff0000);
-    this.physics.add.existing(this.player);
-
-    this.player.body.setCollideWorldBounds(false);
-    this.player.body.setGravityY(900);
-
-    // столкновение
-    this.physics.add.collider(this.player, this.platforms);
-
-    // камера
     this.cameras.main.startFollow(this.player);
-    this.cameras.main.setDeadzone(120, 150);
+    this.cameras.main.setDeadzone(100, 100);
 
-    // управление
-    this.controls = new MobileControls(this);
-
-    // скорость
-    this.speed = 180;
+    this.hud = new HUD(this);
   }
 
-  update() {
-    const body = this.player.body;
-
-    // движение
-    if (this.controls.left) {
-      body.setVelocityX(-this.speed);
-    } else if (this.controls.right) {
-      body.setVelocityX(this.speed);
-    } else {
-      body.setVelocityX(0);
-    }
-
-    // прыжок
-    if (this.controls.jump && body.blocked.down) {
-      body.setVelocityY(-520);
-    }
-
-    // смерть при падении
-    if (this.player.y > this.scale.height + 300) {
-      this.scene.restart();
-    }
+  update(time, delta) {
+    this.inputSystem.update();
+    this.player.update(this.inputSystem);
+    this.hud.update(this.player.hp);
   }
 }
