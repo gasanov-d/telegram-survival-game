2da1a7f4b324001cbb93ece113db88d4bb0932ae
gasanov-d <gasanovali1999@gmail.com>
2025-12-27 20:03:42 +0300

    Update InputSystem.js

--- systems/InputSystem.js
+++ systems/InputSystem.js
@@ -2,10 +2,8 @@ export default class InputSystem {
   constructor(scene) {
     this.scene = scene;
 
-    // Направление движения
     this.direction = { x: 0, y: 0 };
 
-    // Клавиатура
     this.keys = scene.input.keyboard.addKeys({
       up: "W",
       down: "S",
@@ -22,28 +20,16 @@ export default class InputSystem {
     this.direction.x = 0;
     this.direction.y = 0;
 
-    // Горизонталь
-    if (this.keys.left.isDown || this.keys.leftArrow.isDown) {
-      this.direction.x = -1;
-    } else if (this.keys.right.isDown || this.keys.rightArrow.isDown) {
-      this.direction.x = 1;
-    }
+    if (this.keys.left.isDown || this.keys.leftArrow.isDown) this.direction.x = -1;
+    else if (this.keys.right.isDown || this.keys.rightArrow.isDown) this.direction.x = 1;
 
-    // Вертикаль
-    if (this.keys.up.isDown || this.keys.upArrow.isDown) {
-      this.direction.y = -1;
-    } else if (this.keys.down.isDown || this.keys.downArrow.isDown) {
-      this.direction.y = 1;
-    }
+    if (this.keys.up.isDown || this.keys.upArrow.isDown) this.direction.y = -1;
+    else if (this.keys.down.isDown || this.keys.downArrow.isDown) this.direction.y = 1;
 
-    // Нормализация (чтобы диагональ не была быстрее)
     if (this.direction.x !== 0 && this.direction.y !== 0) {
-      const length = Math.sqrt(
-        this.direction.x * this.direction.x +
-        this.direction.y * this.direction.y
-      );
-      this.direction.x /= length;
-      this.direction.y /= length;
+      const len = Math.hypot(this.direction.x, this.direction.y);
+      this.direction.x /= len;
+      this.direction.y /= len;
     }
   }
 
