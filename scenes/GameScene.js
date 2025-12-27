2f3e9a9396f44579ef9b90b0437f2eabf017dcc4
gasanov-d <gasanovali1999@gmail.com>
2025-12-27 18:58:19 +0300

    Update GameScene.js

--- scenes/GameScene.js
+++ scenes/GameScene.js
@@ -1,5 +1,4 @@
-import MobileControls from './MobileControls.js';
-import PlatformManager from '../world/PlatformManager.js';
+import MobileControls from '../controls/MobileControls.js';
 
 export default class GameScene extends Phaser.Scene {
   constructor() {
@@ -7,64 +6,63 @@ export default class GameScene extends Phaser.Scene {
   }
 
   create() {
+    const w = this.scale.width;
     const h = this.scale.height;
 
+    // фон
     this.cameras.main.setBackgroundColor('#6fa8dc');
-    this.physics.world.setBounds(0, 0, 100000, h);
+
+    // границы мира
+    this.physics.world.setBounds(0, 0, 10000, h);
+
+    // платформа (земля)
+    this.platforms = this.physics.add.staticGroup();
+    this.platforms
+      .create(5000, h - 20)
+      .setDisplaySize(10000, 40)
+      .refreshBody();
 
     // игрок
-    this.player = this.add.rectangle(100, h - 200, 40, 40, 0xff0000);
+    this.player = this.add.rectangle(100, h - 100, 40, 40, 0xff0000);
     this.physics.add.existing(this.player);
+
+    this.player.body.setCollideWorldBounds(false);
     this.player.body.setGravityY(900);
 
-    // управление
-    this.controls = new MobileControls(this);
-
-    // платформы
-    this.platformManager = new PlatformManager(this);
-    this.physics.add.collider(this.player, this.platformManager.platforms);
+    // столкновение
+    this.physics.add.collider(this.player, this.platforms);
 
     // камера
     this.cameras.main.startFollow(this.player);
     this.cameras.main.setDeadzone(120, 150);
 
-    // скорость
-    this.baseSpeed = 180;
-    this.currentSpeed = this.baseSpeed;
+    // управление
+    this.controls = new MobileControls(this);
 
-    // очки
-    this.startX = this.player.x;
-    this.scoreText = this.add.text(16, 16, '0', {
-      fontSize: '28px',
-      color: '#fff'
-    }).setScrollFactor(0);
- this.scene.launch('UIScene');
- }
+    // скорость
+    this.speed = 180;
+  }
 
   update() {
     const body = this.player.body;
 
-    // ОБЯЗАТЕЛЬНО
-    this.controls.update();
-
     // движение
     if (this.controls.left) {
-      this.currentSpeed = Math.max(80, this.currentSpeed - 6);
+      body.setVelocityX(-this.speed);
     } else if (this.controls.right) {
-      this.currentSpeed += 6;
+      body.setVelocityX(this.speed);
+    } else {
+      body.setVelocityX(0);
     }
-
-    body.setVelocityX(this.currentSpeed);
 
     // прыжок
     if (this.controls.jump && body.blocked.down) {
       body.setVelocityY(-520);
     }
 
-    this.platformManager.update();
-
-    this.scoreText.setText(
-      Math.floor(this.player.x - this.startX)
-    );
+    // смерть при падении
+    if (this.player.y > this.scale.height + 300) {
+      this.scene.restart();
+    }
   }
 }
