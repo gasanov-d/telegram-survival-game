4f79439ac04c5043764e98731d3ee90fc1bdeb5a
gasanov-d <gasanovali1999@gmail.com>
2025-12-27 20:07:32 +0300

    Update HUD.js

--- ui/HUD.js
+++ ui/HUD.js
@@ -1,1 +1,16 @@
+export default class HUD {
+  constructor(scene) {
+    this.scene = scene;
 
+    this.hpText = scene.add.text(16, 16, "HP: 100", {
+      fontSize: "18px",
+      fill: "#ffffff"
+    });
+
+    this.hpText.setScrollFactor(0);
+  }
+
+  update(hp) {
+    this.hpText.setText(`HP: ${hp}`);
+  }
+}
