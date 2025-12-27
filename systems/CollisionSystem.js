6a982dd8aaadc6640752253522c7df463dba0b34
gasanov-d <gasanovali1999@gmail.com>
2025-12-27 20:10:21 +0300

    Update CollisionSystem.js

--- systems/CollisionSystem.js
+++ systems/CollisionSystem.js
@@ -1,1 +1,13 @@
+export default class CollisionSystem {
+  constructor(scene, player, enemies) {
+    this.scene = scene;
 
+    scene.physics.add.overlap(
+      player,
+      enemies,
+      () => {
+        player.takeDamage(10);
+      }
+    );
+  }
+}
