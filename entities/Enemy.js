e9efe21df33f25aec3eca410c4225ab1a68eda6b
gasanov-d <gasanovali1999@gmail.com>
2025-12-27 20:40:30 +0300

    Update Enemy.js

--- entities/Enemy.js
+++ entities/Enemy.js
@@ -1,29 +1,28 @@
-export default class Enemy extends Phaser.Physics.Arcade.Sprite {
-  constructor(scene, x, y, target) {
-    super(scene, x, y, null);
+import Enemy from "../entities/Enemy.js";
 
-    scene.physics.add.existing(this);
-    scene.add.existing(this);
+export default class EnemySpawner {
+  constructor(scene, player) {
+    this.scene = scene;
+    this.player = player;
 
-    this.setDisplaySize(32, 32);
-    this.setTint(0xff0000);
+    this.enemies = scene.physics.add.group();
 
-    this.speed = 100;
-    this.target = target;
+    scene.time.addEvent({
+      delay: 2000,
+      loop: true,
+      callback: () => this.spawn()
+    });
+  }
+
+  spawn() {
+    const x = Phaser.Math.Between(0, this.scene.scale.width);
+    const y = Phaser.Math.Between(0, this.scene.scale.height);
+
+    const enemy = new Enemy(this.scene, x, y, this.player);
+    this.enemies.add(enemy);
   }
 
   update() {
-    if (!this.target || !this.target.active) return;
-
-    const dx = this.target.x - this.x;
-    const dy = this.target.y - this.y;
-    const len = Math.hypot(dx, dy);
-
-    if (len === 0) return;
-
-    this.body.setVelocity(
-      (dx / len) * this.speed,
-      (dy / len) * this.speed
-    );
+    this.enemies.getChildren().forEach(e => e.update());
   }
 }
