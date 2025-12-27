f915aa880f2129e63ea1206f6cb992393ce7dbe7
gasanov-d <gasanovali1999@gmail.com>
2025-12-27 21:26:21 +0300

    Update InputSystem.js

--- systems/InputSystem.js
+++ systems/InputSystem.js
@@ -1,37 +1,31 @@
 export default class InputSystem {
   constructor(scene) {
-    this.scene = scene;
-
     this.direction = { x: 0, y: 0 };
 
-    this.keys = scene.input.keyboard.addKeys({
-      up: "W",
-      down: "S",
-      left: "A",
-      right: "D",
-      upArrow: "UP",
-      downArrow: "DOWN",
-      leftArrow: "LEFT",
-      rightArrow: "RIGHT"
+    scene.input.on("pointermove", pointer => {
+      const cx = scene.scale.width / 2;
+      const cy = scene.scale.height / 2;
+
+      const dx = pointer.x - cx;
+      const dy = pointer.y - cy;
+      const len = Math.hypot(dx, dy);
+
+      if (len > 20) {
+        this.direction.x = dx / len;
+        this.direction.y = dy / len;
+      } else {
+        this.direction.x = 0;
+        this.direction.y = 0;
+      }
+    });
+
+    scene.input.on("pointerup", () => {
+      this.direction.x = 0;
+      this.direction.y = 0;
     });
   }
 
-  update() {
-    this.direction.x = 0;
-    this.direction.y = 0;
-
-    if (this.keys.left.isDown || this.keys.leftArrow.isDown) this.direction.x = -1;
-    else if (this.keys.right.isDown || this.keys.rightArrow.isDown) this.direction.x = 1;
-
-    if (this.keys.up.isDown || this.keys.upArrow.isDown) this.direction.y = -1;
-    else if (this.keys.down.isDown || this.keys.downArrow.isDown) this.direction.y = 1;
-
-    if (this.direction.x !== 0 && this.direction.y !== 0) {
-      const len = Math.hypot(this.direction.x, this.direction.y);
-      this.direction.x /= len;
-      this.direction.y /= len;
-    }
-  }
+  update() {}
 
   getDirection() {
     return this.direction;
