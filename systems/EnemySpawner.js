7ed26ed95462caede9d5eec2b2653a547dd9f322
gasanov-d <gasanovali1999@gmail.com>
2025-12-27 20:08:59 +0300

    Update EnemySpawner.js

--- systems/EnemySpawner.js
+++ systems/EnemySpawner.js
@@ -1,1 +1,27 @@
+import Enemy from "../entities/Enemy.js";
 
+export default class EnemySpawner {
+  constructor(scene, player) {
+    this.scene = scene;
+    this.player = player;
+    this.enemies = scene.add.group();
+
+    scene.time.addEvent({
+      delay: 2000,
+      loop: true,
+      callback: () => this.spawn()
+    });
+  }
+
+  spawn() {
+    const x = Phaser.Math.Between(0, this.scene.scale.width);
+    const y = Phaser.Math.Between(0, this.scene.scale.height);
+
+    const enemy = new Enemy(this.scene, x, y, this.player);
+    this.enemies.add(enemy);
+  }
+
+  update() {
+    this.enemies.getChildren().forEach(e => e.update());
+  }
+}
