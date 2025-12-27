94eb3a9bb0946ea5baaee21a9fdc119c5bc769bf
gasanov-d <gasanovali1999@gmail.com>
2025-12-27 20:19:55 +0300

    Update UIScene.js

--- scenes/UIScene.js
+++ scenes/UIScene.js
@@ -1,24 +1,17 @@
+import HUD from "../ui/HUD.js";
+
 export default class UIScene extends Phaser.Scene {
   constructor() {
-    super('UIScene');
+    super("UIScene");
   }
 
   create() {
-    // текст очков
-    this.scoreText = this.add.text(16, 16, '0', {
-      fontSize: '28px',
-      fontFamily: 'Arial',
-      color: '#ffffff'
-    });
+    this.hud = new HUD(this);
 
-    this.scoreText.setScrollFactor(0);
-    this.scoreText.setDepth(1000);
+    const gameScene = this.scene.get("GameScene");
 
-    // слушаем события из GameScene
-    const gameScene = this.scene.get('GameScene');
-
-    gameScene.events.on('score', score => {
-      this.scoreText.setText(score);
+    gameScene.events.on("player-hp", (hp) => {
+      this.hud.update(hp);
     });
   }
 }
