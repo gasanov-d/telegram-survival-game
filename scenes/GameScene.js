042ae7ddd204f6e5248f17f39f80097dbbe6bfbd
gasanov-d <gasanovali1999@gmail.com>
2025-12-27 21:17:34 +0300

    Update GameScene.js

--- scenes/GameScene.js
+++ scenes/GameScene.js
@@ -1,6 +1,7 @@
 import Player from "../entities/Player.js";
 import InputSystem from "../systems/InputSystem.js";
-import HUD from "../ui/HUD.js";
+import EnemySpawner from "../systems/EnemySpawner.js";
+import CollisionSystem from "../systems/CollisionSystem.js";
 
 export default class GameScene extends Phaser.Scene {
   constructor() {
@@ -12,15 +13,20 @@ export default class GameScene extends Phaser.Scene {
 
     this.player = new Player(this, 400, 300);
 
-    this.cameras.main.startFollow(this.player);
-    this.cameras.main.setDeadzone(100, 100);
+    this.enemySpawner = new EnemySpawner(this, this.player);
 
-    this.hud = new HUD(this);
+    this.collisionSystem = new CollisionSystem(
+      this,
+      this.player,
+      this.enemySpawner.enemies
+    );
+
+    this.cameras.main.startFollow(this.player.gameObject);
   }
 
-  update(time, delta) {
+  update() {
     this.inputSystem.update();
     this.player.update(this.inputSystem);
-    this.hud.update(this.player.hp);
+    this.enemySpawner.update();
   }
 }
