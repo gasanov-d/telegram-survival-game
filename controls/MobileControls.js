b57af26fcf4be9e82a88737572ad1ce41d6510fd
gasanov-d <gasanovali1999@gmail.com>
2025-12-27 18:55:25 +0300

    Update MobileControls.js

--- controls/MobileControls.js
+++ controls/MobileControls.js
@@ -6,38 +6,59 @@ export default class MobileControls {
     this.right = false;
     this.jump = false;
 
-    this.createButtons();
+    // мультитач (важно для телефона)
+    this.scene.input.addPointer(3);
+    this.scene.input.setPollAlways();
+
+    this.createZones();
+    this.bindInput();
   }
 
-  createButtons() {
+  createZones() {
     const w = this.scene.scale.width;
     const h = this.scene.scale.height;
 
-    this.leftBtn = this.scene.add
-      .rectangle(80, h - 80, 120, 120, 0x000000, 0.35)
-      .setScrollFactor(0)
-      .setInteractive();
+    this.zones = {
+      left: new Phaser.Geom.Rectangle(0, h - 200, w * 0.3, 200),
+      right: new Phaser.Geom.Rectangle(w * 0.3, h - 200, w * 0.3, 200),
+      jump: new Phaser.Geom.Rectangle(w * 0.6, h - 200, w * 0.4, 200),
+    };
 
-    this.rightBtn = this.scene.add
-      .rectangle(240, h - 80, 120, 120, 0x000000, 0.35)
-      .setScrollFactor(0)
-      .setInteractive();
-
-    this.jumpBtn = this.scene.add
-      .rectangle(w - 100, h - 80, 140, 140, 0x000000, 0.35)
-      .setScrollFactor(0)
-      .setInteractive();
-
-    this.bind(this.leftBtn, 'left');
-    this.bind(this.rightBtn, 'right');
-    this.bind(this.jumpBtn, 'jump');
+    // визуал кнопок (можно потом убрать)
+    this.drawZone(this.zones.left, 0xff0000);
+    this.drawZone(this.zones.right, 0x00ff00);
+    this.drawZone(this.zones.jump, 0x0000ff);
   }
 
-  bind(btn, key) {
-    btn.on('pointerdown', () => (this[key] = true));
-    btn.on('pointerup', () => (this[key] = false));
-    btn.on('pointerout', () => (this[key] = false));
+  drawZone(rect, color) {
+    const g = this.scene.add.graphics();
+    g.fillStyle(color, 0.25);
+    g.fillRect(rect.x, rect.y, rect.width, rect.height);
+    g.setScrollFactor(0);
   }
 
-  update() {}
+  bindInput() {
+    this.scene.input.on('pointerdown', this.handleInput, this);
+    this.scene.input.on('pointermove', this.handleInput, this);
+    this.scene.input.on('pointerup', this.clearInput, this);
+  }
+
+  handleInput(pointer) {
+    const x = pointer.x;
+    const y = pointer.y;
+
+    this.left = Phaser.Geom.Rectangle.Contains(this.zones.left, x, y);
+    this.right = Phaser.Geom.Rectangle.Contains(this.zones.right, x, y);
+    this.jump = Phaser.Geom.Rectangle.Contains(this.zones.jump, x, y);
+  }
+
+  clearInput() {
+    this.left = false;
+    this.right = false;
+    this.jump = false;
+  }
+
+  update() {
+    // оставлено для будущего расширения
+  }
 }
