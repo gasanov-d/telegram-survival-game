dae5c1b65d3f346c04124c43e15db600a657b090
gasanov-d <gasanovali1999@gmail.com>
2025-12-27 21:34:22 +0300

    Update GameScene.js

--- scenes/GameScene.js
+++ scenes/GameScene.js
@@ -12,14 +12,11 @@ export default class GameScene extends Phaser.Scene {
     const w = this.scale.width;
     const h = this.scale.height;
 
-    // ЯВНЫЙ ФОН (чтобы видеть сцену)
     this.add.rectangle(w / 2, h / 2, w, h, 0x222222);
 
-    // ГРАНИЦЫ МИРА
     this.physics.world.setBounds(0, 0, w, h);
 
     this.inputSystem = new InputSystem(this);
-
     this.player = new Player(this, w / 2, h / 2);
 
     this.enemySpawner = new EnemySpawner(this, this.player);
@@ -31,11 +28,9 @@ export default class GameScene extends Phaser.Scene {
     );
 
     this.cameras.main.startFollow(this.player.gameObject);
-    this.cameras.main.setBounds(0, 0, w, h);
   }
 
   update() {
-    this.inputSystem.update();
     this.player.update(this.inputSystem);
     this.enemySpawner.update();
   }
