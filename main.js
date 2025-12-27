858b309274caff7f6604fa14017c3a787109f7fa
gasanov-d <gasanovali1999@gmail.com>
2025-12-27 20:49:15 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
-import { gameConfig } from './config/gameConfig.js';
+import BootScene from "./scenes/BootScene.js";
+import GameScene from "./scenes/GameScene.js";
+import UIScene from "./scenes/UIScene.js";
 
-new Phaser.Game(gameConfig);
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: false
+    }
+  },
+  scene: [BootScene, GameScene, UIScene]
+};
+
+new Phaser.Game(config);
